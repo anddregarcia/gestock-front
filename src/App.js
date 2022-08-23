d6b836b19f15d3423578e32e1a8256666f2f9aec
André Garcia <anddregarcia@gmail.com>
2022-08-22 21:32:50 -0300

    components de login e rotas

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import './App.css';
 import Header from './components/header';
 import { Routes, Route } from "react-router-dom";
 import Login from './components/Login';
-import Index from './components/Index';
+import Index from './components/Index/index';
 function App() {
   return (
    <>
         
      <Header></Header>
      <Routes>
-      <Route path="/" element={<Login />}></Route>
-      <Route path="/index" element={<Index />}></Route>
-
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/index" element={<Index />}></Route>
     </Routes>
      
    </>
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => { 
 
     return( 
-        <h1>
+        <h1 className="andre">
             Temos um header
         </h1>
     )
