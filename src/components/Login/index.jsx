b59d7ad15ebe1332c213dff929be7e61ea67685e
André Garcia <anddregarcia@gmail.com>
2022-08-22 21:32:25 -0300

    components de login e rotas

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from "react-router-dom";
 
-const Login = () => { 
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
 
-    return( 
-        <link to="/index">Login</link>
+    return(
+        <div className=" d-flex flex-column align-items-center">
+            <div className="w-25 d-flex flex-column align-items-center ">
+    
+            <h1>Login</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" onChange={event => setEmail(event.target.value)} />
+                    <Form.Text className="text-muted">
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={event => setPassword(event.target.value)} />
+                </Form.Group>
+                
+                    <Link to="/index">
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+                    </Link>
+            </Form>
+            </div>
+        </div> 
     );
 }
 
